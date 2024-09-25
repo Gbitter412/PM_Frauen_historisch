45a0ed3c35462c789b8e6f3f802f391ef947913a
CanerHSMA <caneraliji020402@gmail.com>
2024-09-25 23:54:29 +0200

    fix: added pictures instead of text

--- site/src/codeCracker.js
+++ site/src/codeCracker.js
@@ -1,52 +1,52 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const riddles = [
     {
-      image: "../assets/img/cc_2.png",
+      image: "../assets/img/cc_1.png",
       answer: "15",
       hint: "Es ist eine einfache Addition.",
     },
     {
-      code: "name=Ada\nprint(name * 3)",
+      image: "../assets/img/cc_2.png",
       answer: "AdaAdaAda",
       hint: "In Python wird eine Zeichenkette mit der Multiplikation durch eine Zahl so oft wiederholt.",
     },
     {
-      code: "a = 10\nb = 5\na, b = b, a\n print(a, b)",
+      image: "../assets/img/cc_3.png",
       answer: "5 10",
       hint: "In Python kannst du Variablen in einer Zeile mithilfe von Tupel-Entpackung tauschen.",
     },
     {
-      code: 'x = 4\nif x % 2 == 0:\n\tprint("Even")\nelse:\n\tprint("Odd") ',
+      image: "../assets/img/cc_4.png",
       answer: "Even",
       hint: "Der %-Operator gibt den Rest zurück. Wenn x % 2 == 0, ist die Zahl gerade.",
     },
     {
-      code: 's = "Python"\nprint(s[2])',
+      image: "../assets/img/cc_5.png",
       answer: "t",
       hint: "Zeichenketten sind in Python nullbasiert. Das erste Zeichen befindet sich an Index 0.",
     },
     {
-      code: "x = [1, 2, 3]\ny = x\ny.append(4)\nprint(x)",
+      image: "../assets/img/cc_6.png",
       answer: "[1, 2, 3, 4]",
       hint: "In Python sind Listen veränderbar, und y verweist auf dieselbe Liste wie x. Klammern nicht vergessen!",
     },
     {
-      code: 'for i in range(1, 6):\n\tif i % 2 == 0:\n\t\tprint(i, end=" ")',
+      image: "../assets/img/cc_7.png",
       answer: "2 4",
       hint: "Die Funktion range(1, 6) erzeugt Zahlen von 1 bis 5. Die if-Bedingung prüft auf gerade Zahlen.",
     },
     {
-      code: "def add(a, b):\n\treturn a + b\nprint(add(2, 3))",
+      image: "../assets/img/cc_8.png",
       answer: "5",
       hint: "Diese Funktion addiert zwei Zahlen und gibt das Ergebnis zurück.",
     },
     {
-      code: "x = [0, 1, 2]\nx.insert(1, 5)\nprint(x)",
+      image: "../assets/img/cc_9.png",
       answer: "[0, 5, 1, 2]",
       hint: "Die Methode .insert() fügt ein Element an einem bestimmten Index ein. Klammern nicht vergessen!",
     },
     {
-      code: "n = 5\nfactorial = 1\nfor i in range(1, n+1):\n\tfactorial *= i \nprint(factorial)",
+      image: "../assets/img/cc_10.png",
       answer: "120",
       hint: "Dies ist die Fakultät von n. Multipliziere alle Zahlen von 1 bis n.",
     },
